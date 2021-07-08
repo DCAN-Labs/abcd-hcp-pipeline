37e6e3c776f3e102f63559f3b89ed2ab18f887c2
Thomas Madison <56737848+madisoth@users.noreply.github.com>
2021-07-08 12:05:52 -0700

    Update SetupEnv.sh
    revert DBP version env var to 4.0.0

--- app/SetupEnv.sh
+++ app/SetupEnv.sh
@@ -40,7 +40,7 @@ export MSMBINDIR=/opt/msm/Ubuntu
 # Set up DCAN Environment Variables
 export MCRROOT=/opt/matlab/v92
 export DCANBOLDPROCDIR=/opt/dcan-tools/dcan_bold_proc
-export DCANBOLDPROCVER=DCANBOLDProc_v4.0.4
+export DCANBOLDPROCVER=DCANBOLDProc_v4.0.0
 export EXECSUMDIR=/opt/dcan-tools/executivesummary
 export CUSTOMCLEANDIR=/opt/dcan-tools/customclean
 export MATLAB_PREFDIR=/tmp/.matlab/R2016b
